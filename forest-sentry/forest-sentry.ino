7294ab2fbbf24a8e0f3d7fca67a590d27fb2857f
jmontecinos <jmontecinos@ofbox.com>
2024-07-03 11:19:22 -0600

    Se sustituyen los pines RX y TX para permitir comunicarse con el modulo GPS sin interferir en el puerto USB

--- forest-sentry/forest-sentry.ino
+++ forest-sentry/forest-sentry.ino
@@ -3,6 +3,8 @@
 #include <WiFiClient.h>
 #include <DHT.h>
 #include <ArduinoJson.h> 
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
 
 //DHT11 Sensor
 #define DHTPIN D3     
@@ -15,19 +17,36 @@ DHT dht(DHTPIN, DHTTYPE);
 // YL-38 Rain Sensor
 #define RAINPIN D2
 
+// Definir pines para GPS
+static const int GPS_RXPin = D4;  // Pin para recibir datos del GPS
+static const int GPS_TXPin = D5;  // Pin para enviar datos al GPS
+static const uint32_t GPSBaud = 9600;
+
+// Crear objeto TinyGPS++
+TinyGPSPlus gps;
+
+// Crear objeto SoftwareSerial
+SoftwareSerial gpsSerial(GPS_RXPin, GPS_TXPin);
 
 /************************* WiFi Access Point *********************************/
 
-#define WLAN_SSID       "<wlan-ssid>"
-#define WLAN_PASS       "<wlan-pass>"
+#define WLAN_SSID       "iPhone Josue Montecinos"
+#define WLAN_PASS       "quetimporta"
 
 /************************* Server *********************************/
 #define SERVER_URL      "http://tu-servidor.com/api/datos"  // URL del servidor donde se enviarán los datos
 
 /*************************** Sketch Code ************************************/
 
+
+double latitud = 0.0;
+double longitud = 0.0;
+
 void setup() {
   Serial.begin(115200);
+  gpsSerial.begin(GPSBaud);  // Comunicación con el módulo GPS
+  
+  Serial.println("GPS Test iniciado");
   //****************************************** Connect to WiFi access point.
   Serial.println(); Serial.println();
   Serial.print("Connecting to ");
@@ -44,8 +63,6 @@ void setup() {
   dht.begin();
 }
 
-String nmeaSentence = "";
-String latAndLon = "";
 bool sentenceStart = false;
 
 void loop() {
@@ -54,32 +71,20 @@ void loop() {
   float temperature = dht.readTemperature();
   int smokeLevel = analogRead(MQ2PIN);
   int rainStatus = digitalRead(RAINPIN);
-  double latitud = 0.0;
-  double longitud = 0.0;
 
   // Evaluar el nivel de alerta
   int alertLevel = evaluateAlertLevel(temperature, humidity, smokeLevel, rainStatus);
-  
+
   // Leer datos del GPS
-  while (Serial.available()) {
-    char c = Serial.read();
-
-    // Iniciar la captura de la oración al encontrar el signo '$'
-    if (c == '$') {
-      sentenceStart = true;
-      nmeaSentence = ""; // Reiniciar la oración
-    }
-
-    // Si la captura ha comenzado, acumula los caracteres
-    if (sentenceStart) {
-      nmeaSentence += c;
-    }
-
-    // Al encontrar el fin de línea, procesar la oración completa
-    if (c == '\n' && sentenceStart) {
-      sentenceStart = false;
-      processNMEASentence(nmeaSentence);
-    }
+  while (gpsSerial.available() > 0) {
+    gps.encode(gpsSerial.read());
+  }
+  // Si se ha obtenido una nueva localización, mostrarla
+  if (gps.location.isUpdated()) {
+    Serial.print("Latitud: ");
+    Serial.println(gps.location.lat(), 6);
+    Serial.print("Longitud: ");
+    Serial.println(gps.location.lng(), 6);
   }
 
   // Preparar el JSON con los datos
@@ -100,8 +105,6 @@ void loop() {
   Serial.println("Humedad: " + String(humidity));
   Serial.println("Nivel de Gas: " + String(smokeLevel));
   Serial.println("Lluvia: " + interpretRainSensorDigital(rainStatus));
-  Serial.println("NMEASentence: " + nmeaSentence);
-  Serial.println("Lat and Lon: " + latAndLon);
 
 
   // Enviar los datos al servidor
@@ -113,76 +116,14 @@ void loop() {
     int httpResponseCode = http.POST(jsonData);
     if (httpResponseCode > 0) {
       String response = http.getString();
-      Serial.println(response);
+      // Serial.println(response);
     } else {
       Serial.print("Error en el envío: ");
-      Serial.println(httpResponseCode);
+      // Serial.println(httpResponseCode);
     }
     http.end();
   }
-  delay(60000);  // Esperar un minuto antes de enviar el siguiente conjunto de datos
-}
-
-void processNMEASentence(String sentence) {
-  double lat = 0.0;
-  double lon = 0.0;
-
-  if (sentence.startsWith("$GPGGA") || sentence.startsWith("$GPGLL") || sentence.startsWith("$GPRMC")) {
-    // Divide la oración en campos usando la coma como delimitador
-    int fieldIndex = 0;
-    String fields[15]; // Preparado para hasta 15 campos
-
-    int startIndex = 0;
-    int endIndex = -1;
-    while ((endIndex = sentence.indexOf(',', startIndex)) != -1) {
-      fields[fieldIndex++] = sentence.substring(startIndex, endIndex);
-      startIndex = endIndex + 1;
-    }
-    fields[fieldIndex] = sentence.substring(startIndex); // Último campo
-
-    // Extrae los campos relevantes dependiendo del tipo de oración
-    String latitude, latDirection, longitude, longDirection;
-    if (sentence.startsWith("$GPGGA")) {
-      latitude = fields[2];
-      latDirection = fields[3];
-      longitude = fields[4];
-      longDirection = fields[5];
-    } else if (sentence.startsWith("$GPGLL")) {
-      latitude = fields[1];
-      latDirection = fields[2];
-      longitude = fields[3];
-      longDirection = fields[4];
-    } else if (sentence.startsWith("$GPRMC")) {
-      latitude = fields[3];
-      latDirection = fields[4];
-      longitude = fields[5];
-      longDirection = fields[6];
-    }
-
-    // Convierte la latitud y la longitud a grados decimales
-    lat = convertToDecimal(latitude, latDirection);
-    lon = convertToDecimal(longitude, longDirection);
-
-    // Prepara los datos para la publicación
-    latAndLon = String(lat, 6) + "," + String(lon, 6); // 6 dígitos de precisión
-
-    // Actualiza las variables globales
-    latitud = lat;
-    longitud = lon;
-  }
-}
-
-double convertToDecimal(String degreeMinute, String direction) {
-  double rawValue = degreeMinute.toDouble();
-  double degrees = int(rawValue / 100);
-  double minutes = rawValue - (degrees * 100);
-  double decimal = degrees + (minutes / 60);
-
-  if (direction == "S" || direction == "W") {
-    decimal *= -1;
-  }
-
-  return decimal;
+  delay(1000);  // Esperar un minuto antes de enviar el siguiente conjunto de datos
 }
 
 String interpretRainSensorDigital(int sensor) {
