c710bac2a43332766d17de10fbcecec74cf99cf9
Gabriel Martinez <gabriel.emv411@gmail.com>
2024-07-03 18:19:38 -0600

    Fix: Record saving changed to work with JSON

--- backend/alpha/src/main/java/com/forestsentry/SentryAPI/controllers/MainController.java
+++ backend/alpha/src/main/java/com/forestsentry/SentryAPI/controllers/MainController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,8 +35,8 @@ public class MainController {
     @Autowired
     private RecordService recordService;
 
-    @PostMapping("/")
-    public ResponseEntity<?> saveRecord(@ModelAttribute @Valid RecordDTO dto, BindingResult result){
+    @PostMapping(value = "/", consumes = "application/json")
+    public ResponseEntity<?> saveRecord(@RequestBody @Valid RecordDTO dto, BindingResult result){
         try{
             if(result.hasErrors()){
                 return new ResponseEntity<>(result.getAllErrors(), HttpStatus.BAD_REQUEST);
--- backend/alpha/src/main/java/com/forestsentry/SentryAPI/models/dtos/RecordDTO.java
+++ backend/alpha/src/main/java/com/forestsentry/SentryAPI/models/dtos/RecordDTO.java
@@ -7,13 +7,17 @@ import org.springframework.lang.Nullable;
 import com.forestsentry.SentryAPI.models.entities.Record;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
 import jakarta.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Jacksonized
 public class RecordDTO {
     @NotNull
     private Float latitude;
